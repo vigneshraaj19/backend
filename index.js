7ed55be691ec9091b4181080fa7e30d3105df778
vigneshraaj <vigneshraaj19@gmail.com>
2022-08-12 20:08:15 +0530

    update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use('/api',userRouter);
 
-app.get('/vicky',(req,res) => res.send("hello vignesh raaj"));
+app.get('/',(req,res) => res.send("hello vignesh raaj"));
 const port=process.env.PORT || 5000;
 app.listen(port,() =>{
     console.log("localhost connected sucessfully");
